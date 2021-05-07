bb1d7b7d4acb4e7d574f0eb2c01c9c8d2593906f
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-05-07 18:34:16 -0500

    updates

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-let items = [];
+let items = [];//date date store
+let workItems = []; //Work item date store
 
+//template vs layout
+//root route for the date
 app.get("/", function(req, res){
 
    let today = new Date();
@@ -19,21 +22,44 @@ app.get("/", function(req, res){
   let options = { weekday: 'long', day: 'numeric' , month: 'long' };
 
 
-  console.log(today.toLocaleDateString("en-US")); // 9/17/2016
-  console.log(today.toLocaleDateString("en-US", options)); // Saturday, September 17, 2016
+  // console.log(today.toLocaleDateString("en-US")); // 9/17/2016
+//  console.log(today.toLocaleDateString("en-US", options)); // Saturday, September 17, 2016
 day= today.toLocaleDateString("en-US", options);
 
 console.log(today.toDateString());
-res.render('list', {kindOfDay: day, newItems: items});
+res.render('list', {listTitle: day, newItems: items});
+
+});
+
+//work route
+app.get("/work", function(req, res){
+res.render('list',  {listTitle:"Work List", newItems: workItems});
 
 });
 
 app.post("/", function(req, res){
-item = req.body.userInput;
-items.push(item);
-console.log(item);
-res.redirect("/");
+  console.log(req.body);
 
+item = req.body.userInput;
+if(req.body.list === "Work"){
+  workItems.push(item);
+  res.redirect("/work");
+
+}else{
+  items.push(item);
+  // console.log(item);
+  res.redirect("/");
+
+}
+
+
+})
+
+app.post("/work", function(req, res){
+
+  let item = req.body.userInput;
+  workItems.push(item);
+  res.redirect("/work");
 
 })
 
